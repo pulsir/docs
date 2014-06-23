feb7c6912f3aa659b2a007c83fab3ba006b83d0e
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-06-24 00:32:57 +0200

    Create footer.php

---
+++ src/theme/footer.php
@@ -0,0 +1,10 @@
+    <div class="footer">
+<h3>Need help? We're here for you.</h3>
+<div><a href="index.php?file=contact" class="nav_button">Talk to a human</a></div>
+      <hr><p>Coded out with love by <a href="http://pulsir.eu">the Pulsir team</a> and released under the terms of <a href="http://license.visualidiot.com">the :) license</a>. Have anything to add? <a href="http://github.com/pulsir/docs">This site is on GitHub</a>.</p>
+    </div>
+</div>
+
+    </div>
+  </body>
+</html>
