b29bf131057b3e37d8dedd11f9048f959961f045
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-06-24 00:33:58 +0200

    Create index.php

---
+++ storage/index.php
@@ -0,0 +1,8 @@
+<h2>Need help? <b>We're here for you.</b></h2>
+<p>Be short and direct. 140 characters or less is awesome.</p>
+<script type="text/javascript" src="http://assets.freshdesk.com/widget/freshwidget.js"></script>
+<style type="text/css" media="screen, projection">
+	@import url(http://assets.freshdesk.com/widget/freshwidget.css); 
+</style> 
+<iframe class="freshwidget-embedded-form" id="freshwidget-embedded-form" src="http://pulsirsupport.freshdesk.com/widgets/feedback_widget/new?&widgetType=embedded&formTitle=Talk+to+a+human&submitThanks=Lookin'+great.+We'll+reply+as+soon+as+we+can.&screenshot=no&attachFile=no&searchArea=no" scrolling="no" height="500px" width="100%" frameborder="0" >
+</iframe>
