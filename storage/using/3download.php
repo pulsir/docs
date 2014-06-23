071f8ae7cca6b9de1d75e04ba6ae64fe327fcc03
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-06-24 00:35:37 +0200

    Create 3download.php

---
+++ storage/using/3download.php
@@ -0,0 +1,5 @@
+<h1>Installing Pulsir locally</h1>
+<p>Okay, this documentation is written for Pulsir 3.0.2 beta, which at the moment I'm writing this, isn't available on GitHub, but will be available soon. At the moment, it's only running on <a href="http://area51.pulsir.eu">Area 51</a>, the Pulsir beta testing facility.</p>
+<p>First of all, you need to grab the code from GitHub:</p>
+<code>git clone https://github.com/pulsir/pulsir.git</code>
+<p>Now that you've got the code ready, you need to put it in your webserver's public folder. When you've got that going, we can proceed.</p>
