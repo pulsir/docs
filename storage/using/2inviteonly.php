7e557f7269253f55c3b8e9b11bcc01cdfe202c09
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-06-24 00:35:21 +0200

    Create 2inviteonly.php

---
+++ storage/using/2inviteonly.php
@@ -0,0 +1,13 @@
+<h2>Invite only?</h2>
+<p>Pulsir's hosted variant was invite-only for a while, but now it's available to the public. However, we want to encourage high-quality blogs and may decrease the amount of posts by low-quality bloggers recommended to users.</p>
+<p>This is the original documentation post about this:</p>
+<div class="quote">
+<p>We want to create an awesome community of passionate bloggers and users — that's why we don't let everybody in. You need to create good content and be nice. We don't let trolls, spammers or boring people who write long articles about how their cactus is prettier than someone's cactus in.</p>
+<p>If you don't get a Pulsir blog, don't worry.<br>
+You can:<br><br>
+Ask us nicely to give you a second chance (if you're rude, we won't)<br>
+Use the simple Pulsir features (just posts, nothing else)<br>
+Get the Pulsir source from GitHub and run it yourself, so you can write about cactuses<br>
+We don't want cactus lovers and groupies — that's why we don't let everybody in.<br><br><br><br>
+TL;DR: Be awesome and you'll get in.</p>
+</div> 
