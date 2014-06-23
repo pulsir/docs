910c6a9eccb2ad84b34324705183ec395b17f6a1
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-06-24 00:35:05 +0200

    Create 1openinganaccount.php

---
+++ storage/using/1openinganaccount.php
@@ -0,0 +1,2 @@
+<h2>Using the hosted Pulsir</h2>
+<p><a href="http://pulsir.eu">Pulsir</a> is free and easy to use as a hosted platform. You can get an account at <a href="http://pulsir.eu">pulsir.eu</a> for free, and you'll be up and ready in a minute.</p>
