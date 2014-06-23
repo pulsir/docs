d9aee75596b07ca15452ff8affe67ed4481c04d5
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-06-24 00:32:40 +0200

    Create header.php

---
+++ src/theme/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Pulsir documentation</title>
+  <link rel="stylesheet" href="http://docs.pulsir.eu/src/theme/style.css">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="header" id="header">
+<div class="container">
+<div class="logo"><a href="http://pulsir.eu"><img src="http://d.pulsir.eu/assets/midres-final2-logo.png" alt="Pulsir" class="logo"></a></div>
+<div class="nav">
+<div class="nav_triangle"></div>
+<div class="nav_links"><a href="index.php">Documentation home</a></div>
+<div class="nav_links"><a href="index.php?file=start">Getting started</a></div>
+<div class="nav_links"><a href="index.php?file=using">Using Pulsir</a></div>
+<div class="nav_links"><a href="http://github.com/pulsir/pulsir">Pulsir on GitHub</a></div> 
+<div class="nav_links"><a href="index.php?file=contact" class="nav_button">Talk to a human</a></div>
+        </div>
+    </div>
+  </div>
+<div class="main">
+  <div class="container">
