029a094023d6f68de234d31cb0462dcf71d901a5
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-06-24 00:30:54 +0200

    Create home.php
    def. horror - this file

---
+++ home.php
@@ -0,0 +1,6 @@
+<h2><i class="fa fa-life-ring"></i> Need help with Pulsir?</h2>
+<h3><a href="http://docs.pulsir.eu/index.php?file=start">Getting started with Pulsir</a></h3>
+<a href="http://docs.pulsir.eu/index.php?file=start#99f22c0c8d9745cc4f9a9f02f985fde1"><i class="fa fa-chevron-right"></i> Using the hosted Pulsir</a><br>
+<a href="http://docs.pulsir.eu/index.php?file=start#39b8f6e45b00f7c650372411928d4d27"><i class="fa fa-chevron-right"></i> Invite only?</a><br>
+<a href="http://docs.pulsir.eu/index.php?file=start#74c3312b9ddd27b021a38e5f8d6d1ab0"><i class="fa fa-chevron-right"></i> Installing Pulsir locally</a><br>
+<small>Subarticles: <a href="http://docs.pulsir.eu/index.php?file=start#9b4eaa9681318eba4fc5927713e9e1c3">Requirements for installing Pulsir locally</a>, <a href="http://docs.pulsir.eu/index.php?file=start#9b4eaa9681318eba4fc5927713e9e1c3">Configuring basic things</a>, <a href="http://docs.pulsir.eu/index.php?file=start#9b4eaa9681318eba4fc5927713e9e1c3">Database and other things</a></small>
