6b9132db4cd45152f2d66076a435cc9255a50d40
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-06-25 16:23:08 +0200

    Create copyright.php

---
+++ storage/legal/copyright.php
@@ -0,0 +1,31 @@
+<header>
+<h3>Copyright violation policy</h3>
+<p>Describes what do we consider to be copyright infringement, what copyright holders can do about it, and what you can do about it.</p>
+</header>
+
+<article>
+
+<p>If you believe that content on Pulsir infringes your copyright(s), you can request removal and/or notify us about it using the methods below. (If you just want attribution, we can do that as well) If we take action about your copyright notice (and we probably will), we'll try to inform the user about it, either using the email address they have provided us with or alerting them when they log in.</p>
+
+<p>Before filing a copyright notice, please <ol><li><b>make sure that the blog is actually hosted by Pulsir.</b> Blogs hosted by Pulsir will either have the Pulsir logo/wordmark on them, or say something along the lines <i>Powered by Pulsir</i>.</li><li><b>try resolving the issue with the user.</b> Filing a copyright notice against someone will create tension between the user and you.</li></ol></p>
+
+<p>If the blog is hosted by Pulsir, and the user isn't responding, please contact Pulsir support using the methods below. But first, make sure you include the following:</p>
+<ul>
+<li>An electronic signature of the copyright holder or a person authorized to act on their behalf.</li>
+<li>A reason for wanting the post removed (not needed for attributions)</li>
+<li>The specific post <abbr def="Uniform Resource Locater, the thing that shows up in the address bar">URL</a> (permalink). If you send us an unspecific URL, or don't include one, action will be delayed or not taken.</li>
+<li>Your name and a way for us to contact us (preferably email, in no case phone/fax/postal mail)</li>
+<li>A statement that: (a) that you believe in good faith that the use of the content that you claim to infringe your copyright is not authorized by law, or by the copyright owner or such owner's agent;<br><br> (b) that all of the information contained in your Infringement Notice is accurate, and <br><br>(c) that you are either the copyright owner or a person authorized to act on their behalf.</li>
+
+<p>Do not submit false notices, especially as that can be punishable by law.</p>
+
+<p>If that's OK, you can submit a copyright infringement notice to:</p>
+<address>
+Pulsir support<br>
+say.hello@pulsir.eu
+</address>
+
+<p>If an copyright infringement notice is valid, we are required by law to respond to it by disabling access to the (allegedly) infringing content. If you are a Pulsir user and access to portions of your blog have been disabled for this reason, we will notify you. You'll then have the option to send us a written counter-notice stating why you believe content does not infringe copyrights. Do note, that when we get a notice that's valid, we <b>delete</b> the content from our servers.</p>
+
+<p>We'll post copyright notices we get here.</p>
+</article>
