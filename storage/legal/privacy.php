55291a4aa0331b9122b5d555c33b5c4485f3b2ba
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-06-25 16:22:41 +0200

    Create privacy.php

---
+++ storage/legal/privacy.php
@@ -0,0 +1,92 @@
+<header>
+<h3>Privacy policy</h3>
+<p>Describes how we handle your private and public data, what data we collected, and what you can do about it.</p>
+</header>
+
+<div class="changelog">
+<h3>Recent changes</h3>
+<p><a href="#change1">We now use Awstats for analytics.</a></p>
+<hr>
+</div>
+
+<article>
+<p>Your privacy is a core Pulsir value. We want to create a policy that you feel good about.</p>
+
+<p>We take protecting your privacy seriously, and we want you to understand exactly how and why we use your information. This document is our Privacy Policy, which describes what information we collect and what it's used for. It applies to all interactions with the Pulsir website, including content posted through mobile apps, widgets and other Pulsir website. Some sites may provide an option to log in with your Pulsir account, in which case their privacy policy and terms apply. Parts of our own policy regulating what data we send, still are valid.</p>
+
+<p>Please read this Privacy Policy carefully. Your use of and/or registration with Pulsir will constitute your agreement to our policy. If you cannot agree to the terms and conditions set forth below, please do not use our services. Additionally, our Privacy Policy is a part of our Terms of Service.</p>
+
+<h3>About this policy</h3>
+
+<p>Pulsir is an open platform that allows you to communicate about anything you want on any topic you want. We collect some information in order for you to have an account you can use to participate on the site, and other information that is necessary to make the service work. Our goal in developing our privacy practices is to allow your participation to remain as anonymous as you choose and we don't sell or profit from the information you share with us.</p>
+
+<p>We do take efforts to make sure the platform keeps working. This includes using your information, where necessary, to stop spam and other activities that violate the TOS. We also share your information when we are legally required to do so. Please read this privacy policy carefully. It describes what information we collect, how it is used, and when it is shared so you can make smart decisions about how you use Pulsir. We think it strikes a good balance and respects you -- our users -- who make this whole thing possible. Let us know what you think.</p>
+
+<h3>Data we collect</h3>
+
+<p>When you create an account, you are required to provide a username and password and an email address. We do not log the IP address from which the account is created.</p>
+
+<p>The posts and comments you make on Pulsir are fully deleted, even if made to an archived or private post. This means that, by default, they are deleted from our servers (edit: on request) and will still be accessible after your account is deleted. (edit: unless you want us to delete them as well) Also, we only save the most recent version of comments and posts, so your previous edits, once overwritten or deleted by us, are no longer available or stored by us.</p>
+
+
+
+<p><s>Pulsir does not use tools like Google Analytics. This means we don't know exactly how many people visit Pulsir, but that's something we'll live with, rather than allowing another company to store data about your Pulsir browsing habits - not gonna happen.</s><br><br><a id="change1"></a><span class="updated">Okay, this has changed and you really need to know about it. We now use Awstats to analyze data, and while it does log a few things we'd rather not log (like unresolved IP's), it's a great tool and lets us know about errors that happen and how many people visit and use Pulsir. We thought about developing our own analytics tool, but it'd take quite a lot of time and effort. Still, there's a chance that we'll move to a more privacy-aware tool.</span> <br><br><br>
+Pulsir does not store IP addresses associated with specific posts, comments, and private messages.</p>
+
+<p>As you interact with Pulsir as an accountholder, we save certain information about these interactions and your preferences. To see a list of what we store, you may request a dump of parts of our database. For cookies, please use your browsers element inspector or cookie browser.</p>
+
+<h3>How we use or disclose collected information</h3>
+
+<p>It takes a lot of work to keep our site running and prevent abuse, but we do not use your data in order to facilitate this. Information you fill in is used internally only where necessary to provide our services.</p>
+
+<p>Your private information is never for sale. This means that we will only share your personal data with your consent, and after letting you know what information will be shared and with whom, unless it is otherwise permitted in this policy. We do not sell or otherwise give access to any information collected about our users to any third party.</p>
+
+<p>Anonymous, aggregated information that cannot be linked back to an individual user may be made available to third parties or public. (edit: To clarify, this means browsing figures, bandwidth usage data, number of people using a specific tag -- general, numerical data about Pulsir, <b>not you</b>.)</p>
+
+<p>Pulsir will not disclose your information unless requested by law. We may disclose or preserve for future disclosure your information if we believe, after due consideration, that doing so is reasonably necessary to comply with a law, regulation, or legal request. If we are going to release your information, we will do our best to provide you with notice in advance via email unless we are prohibited by court order from doing so. (Fuck you, NSA)</p>
+
+<p>Your information may be disclosed by us in an emergency or to keep our services running.<br> Other extraordinary circumstances may require disclosure: we may also disclose your information when we believe it's necessary to prevent imminent and serious bodily harm to a person; to address fraud, security, or spam; or to protect our rights or property.</p>
+
+<h3><a id="cp"></a>Cookie policy</h4>
+
+<p>Cookies are small data files placed on your computer by websites you visit in order to help them remember certain information about you. At Pulsir, we set only a few cookies for tracking user preferences and authentication. Each of these uses is described in greater detail below.
+When you log into your account, Pulsir will place a few cookies for the sole purpose of creating the session. It contains your username, login time and hashed authentication information. Some preferences, like Dark mode, may be stored using cookies as well.</p>
+
+<p><b>Cookies we use</b><br><p>We try to use as few cookies as possible here, and leave them for as little as needed.  Basically, we use the following cookies:<p>
+<p><b>darkmode</b> Set only if you enable Dark Mode and it stays there for a year. </p>
+<p><b>plluser</b> When you log in; stays for 3 days. </p>
+<p><b>pllgroup</b> The group you're in, stays there for 3 days after your login. </p>
+<p><b>expon</b> When you log in, we set a cookie with the time of your login (UNIX timestamp) and it stays on your computer for 3 days. </p>
+<p><b>psession</b> When you log in, we set a cookie with a hash of your login data and it stays on your computer for 3 days. </p>
+<p><i>The plluser, pllgroup, expon, psession cookies are removed on logout.</i></p>
+<p><b>pllsession</b> When you visit Pulsir, we put a randomly generated hash to make sure you don't get attacked by horrible people. It stays there for 2700 seconds from each visit. </p>
+<p><b>pcsrfitoken</b> When you do something on Pulsir, we put a cookie used to verify you haven't been tricked into doing it. It stays there for 2700 seconds from each such action. </p>
+<p><b>pat</b> When you post to Pulsir, we store the UNIX timestamp of the time you started writing for 2700 seconds. </p>
+
+<p>Most browsers include an option to clear existing cookies or reject new ones. However, if you do this, portions of the Pulsir website may not function as intended.</p>
+
+<h3>Account deletion</h3>
+
+<p>You may choose to delete your Pulsir account at any time. The usernames associated with deleted accounts remain unavailable for others to use, and your public profile is no longer visible to users of the site. However, the posts and content you made during your tenure as a Pulsir user will not be automatically deleted as part of the account removal process, and your username won't be publicly disassociated with all posts.
+third party sites</p>
+
+<p>Certain third party sites may offer users the option to log in using their Pulsir account (for example, Pulsir's own moderator panel). This option is only an authentication tool and does not transmit any new personal information to Pulsir, from Pulsir to that site (except username and error code),Â Â or give Pulsir access to details of subsequent actions taken on these sites.</p>
+
+<h3>Data security</h3>
+
+<p>Pulsir will make every effort to secure any private information submitted to us by our users. However, no data transmission over the internet is completely secure, so we cannot guarantee the absolute security of this data. You use the service at your own risk, and are responsible for taking reasonable measures to secure your account (such as carefully maintaining the secrecy of your password).</p>
+
+<h3>International visitors</h3>
+
+<p>Pulsir is a Croatia-based development team whose servers are housed in the Croatia. All personal information we collect from our international users is transferred to the Croatia and processed according Croatian law.</p>
+
+<p>We welcome users of all ages, but due to COPPA individuals in the US under the age of 14 may not create an account on Pulsir without parental consent which in that case should be sent by email to Pulsir on say.hello@pulsir.eu -- if you believe someone 13 or younger living in US is using our site without parental consent, please contact us by emailing us at say.hello@pulsir.eu</p>
+
+<h3>Changes to this policy</h3>
+
+<p>We reserve the right to change this policy to meet the changing needs of Pulsir, or for any other reason. If we make changes, we will notify our users. Where the changes substantially alters your rights, notice will appear prominently on your front page. More minor changes may be highlighted on the front page footer.</p>
+
+<h3>Contact</h3>
+
+<p>Pulsir welcomes questions, concerns, and feedback about this policy. Please feel free to drop us an email (it's say.hello@pulsir.eu) or leave a comment.</p>
+</article>
