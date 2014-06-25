a167bdd7cd11ccd7b8d19445430adfa0e63e45ac
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-06-25 16:29:20 +0200

    Update home.php

--- home.php
+++ home.php
@@ -4,3 +4,10 @@
 <a href="http://docs.pulsir.eu/index.php?file=start#39b8f6e45b00f7c650372411928d4d27"><i class="fa fa-chevron-right"></i> Invite only?</a><br>
 <a href="http://docs.pulsir.eu/index.php?file=start#74c3312b9ddd27b021a38e5f8d6d1ab0"><i class="fa fa-chevron-right"></i> Installing Pulsir locally</a><br>
 <small>Subarticles: <a href="http://docs.pulsir.eu/index.php?file=start#9b4eaa9681318eba4fc5927713e9e1c3">Requirements for installing Pulsir locally</a>, <a href="http://docs.pulsir.eu/index.php?file=start#9b4eaa9681318eba4fc5927713e9e1c3">Configuring basic things</a>, <a href="http://docs.pulsir.eu/index.php?file=start#9b4eaa9681318eba4fc5927713e9e1c3">Database and other things</a></small>
+
+
+<h3><a href="http://docs.pulsir.eu/index.php?file=legal">Legal</a></h3>
+<a href="http://docs.pulsir.eu/index.php?file=legal#f36a74956ef73c5a885c22d7ea8a1498"><i class="fa fa-chevron-right"></i> Copyright violation policy</a><br>
+<a href="http://docs.pulsir.eu/index.php?file=legal#08d30e35403e0e87f305ae08ff277e38"><i class="fa fa-chevron-right"></i> Privacy policy</a><br>
+<small>Subarticles: <a href="http://docs.pulsir.eu/index.php?file=legal#cp">Cookie policy</a>
+<a href="http://docs.pulsir.eu/index.php?file=legal#b3a396d002e0b165bbae6719eea9d298"><i class="fa fa-chevron-right"></i> Terms of service </a><br>
