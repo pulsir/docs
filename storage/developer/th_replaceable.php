6d33a2da45c5cc47bbaf36d946504914ff520f07
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-08-22 01:10:31 +0200

    Create th_replaceable.php

---
+++ storage/developer/th_replaceable.php
@@ -0,0 +1,3 @@
+<h3>Replaceable strings</h3>
+<p>Pulsir can replace some theme strings with dynamic content if needed.</p>
+<p>For instance, to get the page title, use {{PAGETITLE}}. For descriptions, {{PAGEDESCRIPTION}}. For the Pulsir installation base URL, {{BASEURL}}.</p>
