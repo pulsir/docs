5d6270b8d653ad3b2d26a538b28c43aef80573ce
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-08-22 01:18:05 +0200

    Create th_1_index.php

---
+++ storage/developer/th_1_index.php
@@ -0,0 +1,2 @@
+<h1>Theming Pulsir</h1>
+<p>Here's the documentation that let's you develop Pulsir themes with more ease. <b>We strongly recommend using a bootstrap like Whitey for developing themes, as our theme engine is quite limited and the documentation just documents functions. You'll have to look at the code and read comments. Sorry :(</b></p>
